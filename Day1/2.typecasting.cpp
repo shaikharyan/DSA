dac8480bf883185db6c658b584a023b66a327706
Aryan Shaikh <arsahiyansk2000@gmail.com>
2023-07-08 13:43:56 +0530

    2.typecasting.cpp

--- Day1/2.typecasting.cpp
+++ Day1/2.typecasting.cpp
@@ -1,3 +1,7 @@
+// In C++, type casting refers to the process of converting the data type of an object or variable into another data type. It allows you to explicitly change the interpretation or representation of a value from one type to another. Type casting can be useful when you need to perform operations that involve different data types or when you want to assign a value of one type to a variable of another type.
+
+
+
 #include <iostream>
 using namespace std;
 int main()
